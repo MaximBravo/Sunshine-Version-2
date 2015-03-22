a8142f1fa19561ff22f3a8ae3061ddcc0b0b57a6
Maxim Bravo <maxim@alexbravo.com>
2015-03-22 14:37:30 -0700

    Renamed com.example.android.sunshine.app to com.maximbravo.upcoming.app

---
+++ app/src/main/java/com/maximbravo/upcoming/app/sync/UpcomingSyncService.java
@@ -0,0 +1,26 @@
+package com.maximbravo.upcoming.app.sync;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+import android.util.Log;
+
+public class UpcomingSyncService extends Service {
+    private static final Object sSyncAdapterLock = new Object();
+    private static UpcomingSyncAdapter sUpcomingSyncAdapter = null;
+
+    @Override
+    public void onCreate() {
+        Log.d("UpcomingSyncService", "onCreate - UpcomingSyncService");
+        synchronized (sSyncAdapterLock) {
+            if (sUpcomingSyncAdapter == null) {
+                sUpcomingSyncAdapter = new UpcomingSyncAdapter(getApplicationContext(), true);
+            }
+        }
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return sUpcomingSyncAdapter.getSyncAdapterBinder();
+    }
+}
